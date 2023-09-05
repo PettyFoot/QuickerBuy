8dd5cba6d439d7b7e04dfaa99aa0f926ce66d31d
Louis Correra <Louis@Correra.org>
2023-09-05 11:01:08 -0400

    setting up nextJS

---
+++ quickerbuy/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/button'
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <div>
+      <Button size="default" variant={'destructive'}>Click</Button>
+    </div>
+
+  )
+}
